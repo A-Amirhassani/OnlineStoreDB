138da0448041e7cd6fb9b423a6b95dc812939ab9
aliamirhassani <Ali.Amirhassani1@gmail.com>
2023-03-22 14:10:16 -0700

    Update React rendering to use createRoot() instead of ReactDOM.render()
    Replaced the use of ReactDOM.render() with createRoot() in the React application to ensure compatibility with React 18 and provide improvements for Concurrent Mode. This change improves the rendering performance and is recommended for all React applications using version 18 or higher.

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './App.css';
 import App from './App';
+import { createRoot } from 'react-dom/client';
  
-ReactDOM.render(
-   <React.StrictMode>
-      <App />
-   </React.StrictMode>,
-   document.getElementById('root')
+
+
+createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
